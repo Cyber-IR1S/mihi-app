b7489c05b9b71e132b01d5828bdde4bccad41f83
IR1S <remma_kis@icloud.com>
2025-10-26 17:54:28 +0900

    Postモデルを管理画面 (admin.py) に登録

--- diary/admin.py
+++ diary/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from django.contrib import admin
+from .models import Post  # 1. さっき作ったPostモデルをインポート
+
+# 2. Postモデルを管理画面に登録する
+admin.site.register(Post)
+
+#superuserのユーザーネーム:IR1S,password:rem0-rem0
